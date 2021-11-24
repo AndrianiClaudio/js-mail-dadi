dd867570508c790df9e1c0fb2b489e2445dc462d
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-25 00:39:49 +0100

    bonus: mail: aggiungi mail non presente a lista

--- Gioco-dei-dadi/js/script.js
+++ Gioco-dei-dadi/js/script.js
@@ -1,12 +1,21 @@
-//Genera due numeri random da 1 a 6
-const numPc = Math.floor(Math.random() * 6) + 1;
-const numUser = Math.floor(Math.random() * 6) + 1;
-const divPrint = document.getElementById('print-result');
-console.log(numPc,numUser);
-if(numPc == numUser) {
-    divPrint.innerHTML = 'I numeri sono uguali';
-} else if(numPc > numUser) {
-    divPrint.innerHTML = 'Il numero del giocatore é minore del numero del PC';
-} else {
-    divPrint.innerHTML = 'Il numero del giocatore é maggiore del numero del PC';
-}
+const btnGen = document.getElementById('number-gen');
+btnGen.addEventListener('click', function () {
+    //Genera due numeri random da 1 a 6
+    const numPc = Math.floor(Math.random() * 6) + 1;
+    const numUser = Math.floor(Math.random() * 6) + 1;
+    // Genera testo con valori numeri
+    let text = 'Numero computer: ' + numPc + ' - Numero giocatore: ' + numUser;
+    //stampo nel div apposito
+    const numPrint = document.getElementById('number-print');
+    numPrint.innerHTML = text;
+    if (numPc == numUser) {
+        text = 'I numeri sono uguali';
+    } else if (numPc > numUser) {
+        text = 'Il numero del giocatore é minore del numero del PC';
+    } else {
+        text = 'Il numero del giocatore é maggiore del numero del PC';
+    }
+    // Div che conterrá stampa risultato
+    const divPrint = document.getElementById('print-result');
+    divPrint.innerHTML = text;
+});
--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,18 +1,20 @@
 // Definisco una lista che conterrá i nomi di chi puó accedere
-const list = ['user1@gmail.com', 'user2@gmail.com','user3@gmail.com','user4@gmail.com'];
-const printDiv = document.getElementById('print-email-find');
-// Chiedo all'utente la sua mail
-const email = prompt('Inserisci la tua email');
-let emailFind = false;
-//verifico presenza email nella mia lista
-for (let i = 0;i < list.length;i++) {
-    if(email == list[i]) {
-        emailFind = true;
+const list = ['user1@gmail.com', 'user2@gmail.com', 'user3@gmail.com', 'user4@gmail.com'];
+// rimangio in attesa del click sul bottone
+const button = document.getElementById('submit-mail');
+// Evento:in attesa del click sul bottone
+button.addEventListener('click', function (){
+    //ottengo email digitata da utente
+    const email = document.getElementById('email-input').value;
+    //leggo la lista e controllo se la mail é presente
+    let text = 'La tua email non risulta essere presente nella lista';
+    for (let i = 0;i < list.length;i++) {
+        //per ogni elemento della lista controllo se é presente il valore. aggiunto un break per non usare un do - while / while
+        if(email == list[i]) {
+            text = 'la tua email risulta essere presente nella lista';
+            break;
+        }
     }
-}
-//stampa se trovato
-if(emailFind) {
-    printDiv.innerHTML = 'Email trovata con successo';
-} else { //stampo che non é stata trocata la email
-    printDiv.innerHTML = 'Email non trovata';
-}
+    const printDiv = document.getElementById('print-email-find');
+    printDiv.innerHTML = text;
+});
